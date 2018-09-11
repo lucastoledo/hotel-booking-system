04c859fbe72aa86ab72777cdc1852d41e2b259ed
Lucas Toledo <lucas@localhost.localdomain>
2018-09-11 15:09:46 +0100

    intermediate configuration server for git

--- hotel-reservations-service/src/main/java/com/booking/system/hotel/reservations/Application.java
+++ hotel-reservations-service/src/main/java/com/booking/system/hotel/reservations/Application.java
@@ -2,12 +2,14 @@ package com.booking.system.hotel.reservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 /**
  * this is the bootstrap class for the entire microservice
  * @author lucas
  *
  */
-@SpringBootApplication 
+@SpringBootApplication
+@RefreshScope
 public class Application { 
 	
 	public static void main(String[] args) {
